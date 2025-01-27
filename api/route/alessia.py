9ef92217039aaf8f6f281ca4e52dcc54cf0e8f53
Henrique Hott <cs.henriquemchott@gmail.com>
2025-01-26 21:14:26 -0300

    feat: running (de)activate-leads in a background task

--- api/route/alessia.py
+++ api/route/alessia.py
@@ -1,16 +1,20 @@
-import logging, typing as t, zipfile
+import io, json, logging, typing as t, zipfile
 
 import fastapi
 import pydantic
 
 import openpyxl
+import requests
 
 from api.APIConfig import APIConfig
 
-from api.controller.ClientController import ClientController
+from api.persistence.connector import get_redis_db
+
 from api.controller.Controller import Controller
+
+from api.controller.ClientController import ClientController
 from api.controller.LeadController import LeadController
-from api.exception.APIException import APIException
+
 from api.exception.ThirdPartyError import ThirdPartyError
 
 from api.thirdparty.UnipileService import UnipileService
@@ -23,88 +27,98 @@ logger = logging.getLogger(__name__)
 
 router = fastapi.APIRouter(prefix="/alessia", tags=[ "Alessia" ])
 
-
 @router.post("/activate-leads")
-async def activate_leads(file: fastapi.UploadFile, pi_api_token: t.Annotated[pydantic.UUID4, fastapi.Header()]):
-    validate_campaing_file(file.file)
+async def activate_leads(file: fastapi.UploadFile,
+                         pi_api_token: t.Annotated[pydantic.UUID4, fastapi.Header()],
+                         background_tasks: fastapi.BackgroundTasks):
+    _bytes = file.file.read();
 
-    sheet = openpyxl.open(file.file)["Sheet"]
+    validate_campaing_file(_bytes)
 
-    client = ClientController.get_by_api_token(api_token=pi_api_token)
-
-    unipile_cfg: dict = APIConfig.get("Unipile")
-
-    unipile = UnipileService(authorization_key=unipile_cfg["AuthorizationKey"],
-                             subdomain=unipile_cfg["Subdomain"],
-                             port=unipile_cfg["Port"])
-
-    for row in sheet.iter_rows(min_row=2, values_only=True):
-        logger.debug(row)
-
-        lead_linkedin_url = str(row[8])
-
-        lead_account_id = lead_linkedin_url.split("/")[-1]
-
-        # INFO: This call can fail and we dont know why. Occured for a specific profile. Maybe privace involved.
-        lead_linkedin_profile: dict = extract_data_from_unipile_retrieve_profile(
-            linkedin_profile=unipile.retrieve_profile(account_retrieving=client.linkedin_account_id, account_id=lead_account_id)
-        )
-
-        lead = Lead(owner=client.email,
-                    linkedin_public_identifier=lead_linkedin_profile["linkedin_identifier"],
-                    first_name=lead_linkedin_profile["first_name"],
-                    last_name=lead_linkedin_profile["last_name"],
-                    emails=lead_linkedin_profile["emails"],
-                    phones=lead_linkedin_profile["phones"],
-                    active=True,
-                    deleted=False,
-                    chat_id=None)
-
-        LeadController.save(lead)
-
-    Controller.save()
-
+    background_tasks.add_task(update_leads, file=_bytes, status=True, api_token=pi_api_token)
 
 @router.post("/deactivate-leads")
-async def deactivate_leads(file: fastapi.UploadFile, pi_api_token: t.Annotated[pydantic.UUID4, fastapi.Header()]):
-    validate_campaing_file(file.file)
+async def deactivate_leads(file: fastapi.UploadFile,
+                           pi_api_token: t.Annotated[pydantic.UUID4, fastapi.Header()],
+                           background_tasks: fastapi.BackgroundTasks):
+    _bytes = file.file.read();
 
-    sheet = openpyxl.open(file.file)["Sheet"]
+    validate_campaing_file(_bytes)
+
+    background_tasks.add_task(update_leads, file=_bytes, status=True, api_token=pi_api_token)
+
+@router.get("/campaign-status")
+async def campaign_status(pi_api_token: t.Annotated[pydantic.UUID4, fastapi.Header()]) -> dict:
+    redis_db = get_redis_db()
 
     client = ClientController.get_by_api_token(api_token=pi_api_token)
+
+    key = f"LEAD-LOADING-{client.email}"
+
+    data = redis_db.get(key)
+
+    if data is None:
+        raise fastapi.HTTPException(status_code=404)
+
+    output =  json.loads(data)
+
+    if output["status"] == "DONE":
+        redis_db.delete(key)
+
+    return output
+
+def update_leads(file: bytes, status: bool, api_token: pydantic.UUID4):
+    redis_db = get_redis_db()
+
+    sheet = openpyxl.open(io.BytesIO(file))["Sheet"]
+
+    client = ClientController.get_by_api_token(api_token=api_token)
 
     unipile_cfg: dict = APIConfig.get("Unipile")
 
     unipile = UnipileService(authorization_key=unipile_cfg["AuthorizationKey"],
-                             subdomain=unipile_cfg["Subdomain"],
-                             port=unipile_cfg["Port"])
+                            subdomain=unipile_cfg["Subdomain"],
+                            port=unipile_cfg["Port"])
 
-    for row in sheet.iter_rows(min_row=2, values_only=True):
+    redis_db.set(f"LEAD-LOADING-{client.email}", json.dumps(dict(status="LOADING")))
+
+    rows = filter(any, list(sheet.iter_rows(min_row=2, values_only=True)))
+
+    failed_leads = list()
+
+    for row in rows:
         lead_linkedin_url = str(row[8])
 
         lead_account_id = lead_linkedin_url.split("/")[-1]
 
-        # INFO: This call can fail and we dont know why. Occured for a specific profile. Maybe privace involved.
-        lead_linkedin_profile: dict = extract_data_from_unipile_retrieve_profile(
-            linkedin_profile=unipile.retrieve_profile(account_retrieving=client.linkedin_account_id, account_id=lead_account_id)
-        )
+        try:
+            # INFO: This call can fail and we dont know why. Occured for a specific profile. Maybe privace involved.
+            lead_linkedin_profile: dict = extract_data_from_unipile_retrieve_profile(
+                linkedin_profile=unipile.retrieve_profile(account_retrieving=client.linkedin_account_id, account_id=lead_account_id)
+            )
 
-        lead = Lead(owner=client.email,
-                    linkedin_public_identifier=lead_linkedin_profile["linkedin_identifier"],
-                    first_name=lead_linkedin_profile["first_name"],
-                    last_name=lead_linkedin_profile["last_name"],
-                    emails=lead_linkedin_profile["emails"],
-                    phones=lead_linkedin_profile["phones"],
-                    active=True,
-                    deleted=False)
+            lead = Lead(owner=client.email,
+                        linkedin_public_identifier=lead_linkedin_profile["linkedin_identifier"],
+                        first_name=lead_linkedin_profile["first_name"],
+                        last_name=lead_linkedin_profile["last_name"],
+                        emails=lead_linkedin_profile["emails"],
+                        phones=lead_linkedin_profile["phones"],
+                        active=status,
+                        deleted=False,
+                        chat_id=None)
 
-        LeadController.save(lead)
+            LeadController.save(lead)
+        except requests.exceptions.HTTPError:
+            failed_leads.append(str(row[1]) + " " + str(row[2]))
+            continue
 
     Controller.save()
 
-def validate_campaing_file(file: t.BinaryIO):
+    redis_db.set(f"LEAD-LOADING-{client.email}", json.dumps(dict(status="DONE", failed_leads=failed_leads)))
+
+def validate_campaing_file(file: bytes):
     try:
-        sheet = openpyxl.open(file)["Sheet"]
+        sheet = openpyxl.open(io.BytesIO(file))["Sheet"]
     except zipfile.BadZipFile:
         raise fastapi.HTTPException(
             status_code=422,
@@ -143,8 +157,6 @@ def validate_campaing_file(file: t.BinaryIO):
             )
 
 def extract_data_from_unipile_retrieve_profile(linkedin_profile: dict) -> dict:
-    logger.debug(linkedin_profile)
-
     extraction = dict()
 
     lead_provider_id: str | None = linkedin_profile.get("provider_id")
